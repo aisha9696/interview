1303ec80140249015d64479ec1f7b5d9f640de35
aisha.abylgazy@bsbnb.kz <Title1609>
2020-11-18 00:08:02 +0600

    assigment 1

--- interview/src/kz/algorithm/Assigment1.java
+++ interview/src/kz/algorithm/Assigment1.java
@@ -2,47 +2,65 @@ package kz.algorithm;
 
 public class Assigment1 {
 
-    int id[];
-    int N;
-    int sz[];
+    private int id[];
+    private int N;
+    private int sz[];
 
-    public Assigment1 ( int[] id, int n, int[] sz ) {
+    public Assigment1(int n) {
         this.id = new int[n];
         N = n;
         this.sz = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            sz[i] = i;
+        }
     }
 
-    boolean connected( int q, int p){
+    boolean connected(int q, int p) {
         return root(q) == root(p);
     }
 
-    int root(int k){
-        while(this.id[k] != k){
+    int root(int k) {
+        while (this.id[k] != k) {
             id[k] = id[id[k]];
             k = id[k];
         }
         return k;
     }
-    void union(int q, int p){
+
+    void union(int q, int p) {
         int i = root(q);
         int j = root(p);
-        if(i == j) return;
-        if(this.sz[i] < this.sz [j]){
-            this.id[i]=j; sz[j]+=sz[i];
-        }else {
-            this.id[j]=i; sz[i]+=sz[j];
+        if (i == j) return;
+        if (this.sz[i] < this.sz[j]) {
+            this.id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            this.id[j] = i;
+            sz[i] += sz[j];
         }
 
     }
 
-    Integer findSmallestTime(   ){
-        return 0;
-    }
 
-    public static void main ( String[] args ) {
-        int[][] member = new  int[][]{{0,5},{1,7},{2,7},{7,4},{8,4},{9,6},{2,3},{3,6},{7,9},{4,5},{0,6},{7,3}};
-        int [] times = new int[]{0,1,2,3,4,5,6,7,8,9,10,11};
+    public static void main(String[] args) {
+        int[][] member = new int[][]{{0, 5}, {1, 7}, {2, 7}, {7, 4}, {8, 4}, {9, 6}, {2, 3}, {3, 6}, {7, 9}, {4, 5}, {0, 6}, {7, 3}};
+        int[] times = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int cout = 0;
+        int N = 10;
+        Assigment1 s = new Assigment1(N);
+        for (int i = 0; i < member.length; i++) {
 
+            int q = member[i][0];
+            int p = member[i][1];
+            if (!s.connected(q, p)) {
+                s.union(q, p);
+                cout++;
+            }else{
+                if (cout == (N - 2)) System.out.println("Early time " + times[i]); break;
+            }
+
+        }
 
     }
 }
---
+++ interview/src/kz/algorithm/Laba7.java
@@ -0,0 +1,21 @@
+package kz.algorithm;
+
+import jdk.internal.org.objectweb.asm.commons.SerialVersionUIDAdder;
+
+public class Laba7 {
+
+    static int even(int N, int count) {
+        if (N == 0) return count;
+        int f = N % 10;
+        if (f % 2 == 0) count++;
+        return even(N / 10, count++);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int N = 987456123;
+        System.out.println(even(N, 0));
+    }
+}
