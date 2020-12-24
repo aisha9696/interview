ec36ba6bd541c77b883698166a056cbbf9ec8f32
aisha.abylgazy@bsbnb.kz <Title1609>
2020-12-24 23:41:10 +0600

    Assigments 1,2

--- interview/src/kz/algorithm/Assigment1.java
+++ interview/src/kz/algorithm/Assigment1.java
@@ -1,66 +1,77 @@
-package kz.algorithm;
+    package kz.algorithm;
 
-public class Assigment1 {
+    import java.util.HashMap;
+    import java.util.Map;
 
-    private int id[];
-    private int N;
-    private int sz[];
+    public class Assigment1 {
 
-    public Assigment1(int n) {
-        this.id = new int[n];
-        N = n;
-        this.sz = new int[n];
-        for (int i = 0; i < n; i++) {
-            id[i] = i;
-            sz[i] = i;
-        }
-    }
+        private int id[];
+        private int N;
+        private int sz[];
+        private int large[];
 
-    boolean connected(int q, int p) {
-        return root(q) == root(p);
-    }
-
-    int root(int k) {
-        while (this.id[k] != k) {
-            id[k] = id[id[k]];
-            k = id[k];
-        }
-        return k;
-    }
-
-    void union(int q, int p) {
-        int i = root(q);
-        int j = root(p);
-        if (i == j) return;
-        if (this.sz[i] < this.sz[j]) {
-            this.id[i] = j;
-            sz[j] += sz[i];
-        } else {
-            this.id[j] = i;
-            sz[i] += sz[j];
+        public Assigment1(int n) {
+            this.id = new int[n];
+            N = n;
+            this.sz = new int[n];
+            for (int i = 0; i < n; i++) {
+                id[i] = i;
+                sz[i] = i;
+                large[i] = i;
+            }
         }
 
-    }
+        boolean connected(int q, int p) {
+            return root(q) == root(p);
+        }
 
+        int root(int k) {
+            while (this.id[k] != k) {
+                id[k] = id[id[k]];
+                k = id[k];
+            }
+            return k;
+        }
 
-    public static void main(String[] args) {
-        int[][] member = new int[][]{{0, 5}, {1, 7}, {2, 7}, {7, 4}, {8, 4}, {9, 6}, {2, 3}, {3, 6}, {7, 9}, {4, 5}, {0, 6}, {7, 3}};
-        int[] times = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int cout = 0;
-        int N = 10;
-        Assigment1 s = new Assigment1(N);
-        for (int i = 0; i < member.length; i++) {
-
-            int q = member[i][0];
-            int p = member[i][1];
-            if (!s.connected(q, p)) {
-                s.union(q, p);
-                cout++;
-            }else{
-                if (cout == (N - 2)) System.out.println("Early time " + times[i]); break;
+        void union(int q, int p) {
+            int i = root(q);
+            int j = root(p);
+            int largestP = large[i];
+            int largestQ = large[j];
+            if (i == j) return;
+            if (this.sz[i] < this.sz[j]) {
+                this.id[i] = j;
+                sz[j] += sz[i];
+                if (largestP > largestQ)
+                large[i] = largestP;
+            } else {
+                this.id[j] = i;
+                sz[i] += sz[j];
+                if (largestQ > largestP)
+                    large[j] = largestQ;
             }
 
         }
 
+
+        public static void main(String[] args) {
+            int[][] member = new int[][]{{0, 5}, {1, 7}, {2, 7}, {7, 4}, {8, 4}, {9, 6}, {2, 3}, {3, 6}, {7, 9}, {4, 5}, {0, 6}, {7, 3}};
+            int[] times = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+            int cout = 0;
+            int N = 10;
+            Assigment1 s = new Assigment1(N);
+            for (int i = 0; i < member.length; i++) {
+
+                int q = member[i][0];
+                int p = member[i][1];
+                if (!s.connected(q, p)) {
+                    s.union(q, p);
+                    cout++;
+                }else{
+                    if (cout == (N - 2)) System.out.println("Early time " + times[i]); break;
+                }
+
+            }
+
+        }
     }
-}
---
+++ interview/src/kz/algorithm/Assigment3.java
@@ -0,0 +1,58 @@
+package kz.algorithm;
+
+public class Assigment3 extends Assigment1{
+    private boolean data[];
+    private Assigment1 as;
+    private int N;
+
+    public Assigment3( int n) {
+        super(n);
+        this.N = N;
+        data = new boolean[N];
+        for (int i = 0; i < N; ++i)
+            data[i] = true;
+        as = new Assigment1(N);
+    }
+    public void remove(int x) {
+        data[x] = false;
+        if (x > 0 && !data[x-1])
+            as.union(x, x-1);
+        if (x < N - 1 && !data[x+1])
+            as.union(x, x+1);
+    }
+
+    public int successor(int x) {
+        if (data[x]) {
+            return x;
+        } else {
+            int res = as.root(x) + 1;
+            if (res >= N) {
+                System.out.println("Error, no successor can be found");
+                return -1;
+            } else {
+                return res;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] member = new int[][]{{0, 5}, {1, 7}, {2, 7}, {7, 4}, {8, 4}, {9, 6}, {2, 3}, {3, 6}, {7, 9}, {4, 5}, {0, 6}, {7, 3}};
+
+        int cout = 0;
+        int N = 10;
+        Assigment3 s = new Assigment3(N);
+        for (int i = 0; i < member.length; i++) {
+
+            int q = member[i][0];
+            int p = member[i][1];
+            if (!s.connected(q, p)) {
+                s.union(q, p);
+
+            }
+
+        }
+        s.remove(7);
+        System.out.println(s.successor(7) == 3);
+
+    }
+}
