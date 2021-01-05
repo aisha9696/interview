3942fe5361030d531214207ac69956064a6f835e
Abylgazy.A <aishaabylgazy1996@gmail.com>
2021-01-05 17:31:30 +0600

    3sum problem

---
+++ interview/src/kz/algorithm/sort/QuickSort.java
@@ -0,0 +1,14 @@
+package kz.algorithm.sort;
+
+public class QuickSort {
+    private int a[];
+
+    public QuickSort(int[] a) {
+        this.a = a;
+    }
+
+    public int[] sortedInt(int a[]){
+
+        return null;
+    }
+}
