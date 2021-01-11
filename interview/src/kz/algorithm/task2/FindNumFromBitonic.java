68fb478c3aa62ecbc916c2270d78d1de5f4cc7f5
aisha.abylgazy@bsbnb.kz <Title1609>
2021-01-11 23:55:59 +0600

    week 1 finishing

--- interview/src/kz/algorithm/task2/FindNumFromBitonic.java
+++ interview/src/kz/algorithm/task2/FindNumFromBitonic.java
@@ -10,10 +10,14 @@ public class FindNumFromBitonic {
         int mid = a.length/2;
 
         if(a[mid-1]>a[mid] || a[mid+1]<a[mid]){
-            return binarySearch.recursiveFunction(a,key, 0,mid-1);
-        }else if(a[mid+1]>a[mid] || a[mid-1]<a[mid]){
-            return binarySearch.descResursiveFunction(a,key,mid+1,a.length-1);
-        }else if(a[mid]==key) {
+            int index= binarySearch.recursiveFunction(a,key, 0,mid-1);
+            if(index!=-1) {return index; };
+        }
+        if(a[mid+1]>a[mid] || a[mid-1]<a[mid]){
+            int index1= binarySearch.descResursiveFunction(a,key,mid+1,a.length-1);
+            if(index1!=-1) {return index1; };
+        }
+        if(a[mid]==key) {
             return mid;
         }
 
@@ -24,7 +28,7 @@ public class FindNumFromBitonic {
     public static void main(String[] args) {
         int []a={3,4,7,8,9,5,2,1};
         FindNumFromBitonic findNumFromBitonic=new FindNumFromBitonic();
-        System.out.println(findNumFromBitonic.findNum(a,5));
+        System.out.println(findNumFromBitonic.findNum(a,1));
     }
 
 }
